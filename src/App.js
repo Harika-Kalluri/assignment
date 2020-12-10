4ff3fc6100e3fb6eb193ca7459accfcb3da9d7fc
Harika-Kalluri <harikatrr@gmail.com>
2020-12-10 14:13:42 +0530

    assignment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import './App.css';
+
+class Header extends Component{
+  render(){
+    return (
+      <div className = "header">
+        Header
+          <div className = "footer">
+            footer
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
